2c5ca435122dc28b79f22f4bf765a0b4be62a9f0
Bryce Steuer <48736452+BryceSteuer@users.noreply.github.com>
2020-01-28 08:00:32 -0600

    Update file
    Changed counter and coordinates slightly

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,10 +1,10 @@
 import turtle 
 step = 0
-while step < 4:
+while step < 5:
     turtle.forward(100)
-    turtle.right(90)
+    turtle.right(95)
     step+=1
 
 for steps in range(4):
     turtle.forward(100)
-    turtle.right(90)
+    turtle.right(95)
